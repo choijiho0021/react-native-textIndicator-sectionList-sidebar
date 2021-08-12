036ccdbf01a131f4b9a4d3aa4b7818a15f02eeb2
choijiho0021 <choijiho0021@gmail.com>
2021-08-12 15:00:53 +0900

    src에 컴포넌트 추가

---
+++ src/components/SectionListSidebar.tsx
@@ -0,0 +1,224 @@
+import React, {forwardRef, memo, useCallback, useEffect, useState} from 'react';
+import {
+  Dimensions,
+  FlatList,
+  ListRenderItem,
+  PixelRatio,
+  SectionList,
+  SectionListData,
+  SectionListProps,
+  StyleProp,
+  StyleSheet,
+  Text,
+  TextProps,
+  TouchableOpacity,
+  View,
+  ViewProps,
+} from 'react-native';
+import SectionListGetItemLayout from 'react-native-section-list-get-item-layout';
+import TextIndicator from './TextIndicator';
+
+const windowHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 3,
+    marginVertical: 4,
+  },
+  sectionHeaderStyle: {
+    justifyContent: 'flex-end',
+    textAlignVertical: 'center',
+    padding: 5,
+    height: 30,
+    fontSize: 14,
+    paddingLeft: 10,
+    backgroundColor: '#ddd',
+  },
+  title: {
+    fontSize: 14,
+  },
+  sidebarItemContainerStyle: {
+    opacity: 0.8,
+    position: 'absolute',
+    bottom: 0,
+    top: 20,
+    right: 0,
+    justifyContent: 'center',
+    backgroundColor: '#ccc',
+    borderRadius: 50,
+    marginHorizontal: 12,
+  },
+  sidebarItemTextStyle: {
+    flex: 1,
+    fontSize: 11,
+    fontWeight: 'bold',
+    color: '#222',
+    justifyContent: 'center',
+    textAlignVertical: 'center',
+  },
+  sidebarItemStyle: {
+    paddingHorizontal: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+interface SectionListDataType {
+  key: string;
+  title: string;
+  data: Array<any>;
+}
+
+interface SectionListSidebarProps extends SectionListProps<any, any> {
+  //general
+  containerStyle?: StyleProp<ViewProps>;
+  rtl?: boolean;
+  //sectionList
+  renderSectionHeader?:
+    | ((info: {
+        section: SectionListData<any, any>;
+      }) => React.ReactElement | null)
+    | undefined;
+  data: SectionListDataType[];
+  sectionHeaderTextStyle?: StyleProp<TextProps>;
+  sectionHeaderStyle?: StyleProp<ViewProps>;
+
+  //getItemList
+  itemHeight?: number;
+  sectionHeaderHeight?: number;
+  footerHeaderHeight?: number;
+  separatorHeight?: number;
+  listHeaderHeight?: number;
+
+  //sidebar
+  renderSidebarItem?: ListRenderItem<string>;
+  sidebarContainerStyle?: StyleProp<ViewProps>;
+  sidebarItemStyle?: StyleProp<ViewProps>;
+  sidebarItemTextStyle?: StyleProp<TextProps>;
+  selectedText?: string;
+  isSelectedShow?: boolean;
+}
+
+const SectionListSidebar = (
+  {
+    rtl = false,
+    sectionHeaderHeight = 30,
+    itemHeight = 30,
+    footerHeaderHeight = 0,
+    separatorHeight = 0,
+    listHeaderHeight = 0,
+    renderSectionHeader = undefined,
+    renderSidebarItem = undefined,
+    containerStyle,
+    sectionHeaderStyle,
+    sidebarContainerStyle,
+    sidebarItemStyle,
+    sidebarItemTextStyle,
+    data,
+    selectedText,
+    isSelectedShow,
+    ...props
+  }: SectionListSidebarProps,
+  ref: React.LegacyRef<SectionList>,
+) => {
+  const [isShow, setIsShow] = useState<boolean>(false);
+  const [indicatorText, setIndicatorText] = useState<string>('');
+
+  useEffect(() => {
+    setIndicatorText(selectedText ?? '');
+    setIsShow(isSelectedShow ?? false);
+  }, [selectedText, isSelectedShow]);
+
+  const sectionKeyExtract = (item, index) => {
+    return item + index;
+  };
+
+  const getItemLayout = SectionListGetItemLayout({
+    getItemHeight: (rowData, sectionIndex, rowIndex) => itemHeight,
+    getSectionHeaderHeight: () => sectionHeaderHeight,
+    getSectionFooterHeight: () => footerHeaderHeight,
+    getSeparatorHeight: () => separatorHeight / PixelRatio.get(),
+    listHeaderHeight: () => listHeaderHeight,
+  });
+
+  const defaultSectionHeader = ({section}: SectionListData<any, any>) => (
+    <Text style={[styles.sectionHeaderStyle, sectionHeaderStyle]}>
+      {section.title}
+    </Text>
+  );
+
+  const jumpToSection = useCallback(
+    (sectionIndex, itemIndex = 0) => {
+      try {
+        ref!.current.scrollToLocation({
+          sectionIndex,
+          itemIndex,
+        });
+      } catch (e) {}
+    },
+    [ref],
+  );
+
+  const defaultSidebarItem = useCallback(
+    ({item, index}) => {
+      return (
+        <View key={item} style={{paddingVertical: 10}}>
+          <TouchableOpacity
+            pressRetentionOffset={{bottom: 5, left: 5, right: 5, top: 5}}
+            onPressIn={() => {
+              jumpToSection(index);
+              setIndicatorText(item);
+              setIsShow(true);
+            }}
+            onPressOut={() => {
+              setIsShow(false);
+            }}
+            hitSlop={{bottom: 10, left: 10, right: 10, top: 10}}
+            style={[styles.sidebarItemStyle, sidebarItemStyle]}>
+            <Text
+              style={[
+                styles.sidebarItemTextStyle,
+                sidebarItemTextStyle,
+                index % 2 === 1 && {fontSize: 5, fontWeight: '900'},
+              ]}>
+              {index % 2 ? '·' : item}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      );
+    },
+    [jumpToSection, sidebarItemStyle, sidebarItemTextStyle],
+  );
+
+  const sidebarKeyExtractor = ({item}: any) => item;
+
+  return (
+    <View style={[styles.container, containerStyle]}>
+      <TextIndicator isShow={isShow} text={indicatorText} />
+      <View style={{flexDirection: rtl === true ? 'row-reverse' : 'row'}}>
+        <SectionList
+          keyExtractor={sectionKeyExtract}
+          getItemLayout={getItemLayout}
+          renderSectionHeader={renderSectionHeader || defaultSectionHeader}
+          ref={ref}
+          {...props}
+        />
+        <View style={[styles.sidebarItemContainerStyle, sidebarContainerStyle]}>
+          <FlatList
+            data={data.map(item => item.key)}
+            scrollEnabled={false}
+            keyExtractor={sidebarKeyExtractor}
+            renderItem={renderSidebarItem || defaultSidebarItem}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default memo(forwardRef(SectionListSidebar));
---
+++ src/components/TextIndicator.tsx
@@ -0,0 +1,44 @@
+import React, {forwardRef, memo} from 'react';
+import {StyleProp, StyleSheet, Text, View, ViewStyle} from 'react-native';
+
+const styles = StyleSheet.create({
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  box: {
+    width: 60,
+    height: 60,
+    opacity: 0.6,
+    borderRadius: 10,
+    backgroundColor: '#eee',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    fontSize: 32,
+  },
+});
+
+interface TextIndicatorProps {
+  isShow: boolean;
+  style?: StyleProp<ViewStyle>;
+  text?: string;
+}
+
+const TextIndicator = ({isShow, style, text}: TextIndicatorProps) => {
+  return isShow ? (
+    <View style={[styles.center, style, {zIndex: 9999}]} collapsable={false}>
+      <View style={styles.box}>
+        {isShow && <Text style={styles.content}>{text}</Text>}
+      </View>
+    </View>
+  ) : null;
+};
+
+export default memo(TextIndicator);
